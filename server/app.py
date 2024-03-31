392ee8ee134d2418767872a5d0e7a4f23d4f99e4
Stephanie Bertram <steph.a.bertram@gmail.com>
2024-03-31 12:39:12 -0700

    complete lab

--- server/app.py
+++ server/app.py
@@ -4,5 +4,43 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    # Initializes an empty string count. This string will be used to accumulate the numbers from the loop.
+    count = f''
+    for x in range(parameter):
+        # Uses an f-string to format the current value of x and append it to the count string.
+        count += f'{x}\n'
+    return count
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    result = None
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return 'Error: Division by zero'
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return 'Invalid operation'
+    return str(result)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
